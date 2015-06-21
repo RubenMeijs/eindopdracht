3a6001f8445bbec0feab39dacb05533b4bd68ff5
kareldewit1 <k.p.dewit@students.uu.nl>
2015-06-21 15:55:39 +0000

    verbetering van evaluate en toevoegen van commentaar

--- expression.py
+++ expression.py
@@ -1,11 +1,11 @@
 import math
-
-# split a string into mathematical tokens
-# returns a list of numbers, operators, parantheses and commas
-# output will not contain spaces
-
+import sys
 
 def tokenize(string):
+    #split a string into mathematical tokens
+    #returns a list of numbers, operators, parantheses and commas
+    #output will not contain spaces
+    
     splitchars = list("+-*/(),")
     
     # surround any splitchar by spaces
@@ -46,16 +46,13 @@ def isint(string):
         return False
 
 class Expression():
-    """A mathematical expression, represented as an expression tree"""
+    """A mathematical expression, represented as an expression tree
     
-    """
     Any concrete subclass of Expression should have these methods:
      - __str__(): return a string representation of the Expression.
      - __eq__(other): tree-equality, check if other represents the same expression tree.
     """
-   
-    # TODO: when adding new methods that should be supported by all subclasses, add them to this list
-    
+
     # operator overloading:
     # this allows us to perform 'arithmetic' with expressions, and obtain another expression
     def __add__(self, other):
@@ -72,21 +69,23 @@ class Expression():
     
     def __pow__(self,other):
         return PowNode(self,other)
-    # Done: other overloads, such as __sub__, __mul__, etc.
-    
+
     # basic Shunting-yard algorithm
     def fromString(self, string):
+        
         # split into tokens
         tokens = tokenize(string)
         
         # stack used by the Shunting-Yard algorithm
         stack = []
+       
         # output of the algorithm: a list representing the formula in RPN
         # this will contain Constant's and '+'s
         output = []
         
         # list of operators
         oplist = ['+','-', '*', '/','**']
+        
         #order_op index 0 is order, index 1 is associativity (0=left, 1=right)
         order_op = {'+':[1,0],'-':[1,0], '*':[2,0], '/':[2,0],'**':[3,1]}
         for token in tokens:
@@ -98,21 +97,15 @@ class Expression():
                 else:
                     output.append(Constant(float(token)))
                     
-            #brain fart komma's worden natuurlijk niet gebruikt om decimalen aan te geven xD        
-            # elif token == ',':
-                
-            #     while stack[-1] != '(' or len(stack)==0:
-            #         output.append(stack.pop())
-                
             elif token in oplist:
-                # pop operators from the stack to the output until the top is no longer an operator
                 
+                # pop operators from the stack to the output until the top is no longer an operator
                 while True:
                     
                     if len(stack) == 0 or stack[-1] not in oplist:
                         break
-                    # Shunting Yard algoritme
                     
+                    # Shunting Yard algoritme
                     elif (order_op[token][1]==0 and order_op[token][0] <= order_op[stack[-1]][0]
                         ) or (order_op[token][1]==1 and order_op[token][0]<order_op[stack[-1]][0]):
                         
@@ -125,27 +118,23 @@ class Expression():
             elif token == '(':
                 # left parantheses go to the stack
                 stack.append(token)
+                
             elif token == ')':
                 # right paranthesis: pop everything upto the last left paranthesis to the output
                 while not stack[-1] == '(':
                     output.append(stack.pop())
+                
                 # pop the left paranthesis from the stack (but not to the output)
                 stack.pop()
-                
-            # TODO: do we need more kinds of tokens?
-            
+
             else:
                 output.append(Variable(token))
-                
-                # volgens mij (ruben) kan valueError weg als we ook variabelen toelaten
-                #else:
-                #     # unknown token
-                #     raise ValueError('Unknown token: %s' % token)
-                            
+
         # pop any tokens still on the stack to the output
         while len(stack) > 0:
             output.append(stack.pop())
         
+        # output is the variable we use to store the RPN representation of an expression
         self.output = output
 
         # convert RPN to an actual expression tree
@@ -160,135 +149,140 @@ class Expression():
                 stack.append(t)
                 
         # the resulting expression tree is what's left on the stack
-        
         return stack[0] 
-    
-    
+
+# Storing constant values  
 class Constant(Expression):
     """Represents a constant value"""
     def __init__(self, value):
         self.value = value
     
-    #waarom hebben we deze functie nodig?    
+    # Overload of equality sign   
     def __eq__(self, other):
         if isinstance(other, Constant):
             return self.value == other.value
         else:
             return False
-        
+    
+    # Overload of tostring   
     def __str__(self):
         return str(self.value)
         
-    # allow conversion to numerical values
+    # allow conversion to int
     def __int__(self):
         return int(self.value)
-        
+    
+    # allow conversion to float   
     def __float__(self):
         return float(self.value)
-        
+    
+    # if evaluation is called, the constant is returned   
     def evaluate(self,variabelen):
+        return self
+    
+    # returns the value without casting a specific type    
+    def constantvalue(self):
         return self.value
         
         
+#hier defineren we de variabelen        
 class Variable(Expression):
-    #hier defineren we de variabelen
+    
+    #initialisatie
     def __init__(self,teken):
         self.teken = teken
     
+    #overloaden van de tostring functie
     def __str__(self):
         return str(self.teken)
     
-    
-    # waarom hebben we een equal functie nodig?
+    #overlaoden van de equals functie
     def __eq__(self,other):
         if isinstance(other, Variable):
             return self.teken == other.teken
         else:
             return False
             
+    #evaluate geeft een constante waarde als x is gesubsitueerd, anders de (ongesubstitueerde) variabele zelf      
     def evaluate(self,variabelen):
         if self.teken in variabelen:
-            return variabelen[self.teken]
+            return Constant(variabelen[self.teken])
         else:
-            return str(self.teken)
+            return self
 
         
 class BinaryNode(Expression):
-    """A node in the expression tree representing a binary operator."""
-    # ik heb nog steeds niet echt een idee wat BinaryNode doet
-    # wat is bijvoorbeeld dat lhs en rhs? waar haalt hij die informatie vandaan
+    #A node in the expression tree representing a binary operator.
     order_op = {'+':[1,True],'-':[1,False], '*':[2,True], '/':[2,False],'**':[3,False]}
-    
-    def __init__(self, lhs, rhs, op_symbol):  #waar roep je deze init aan, waar komen de gegevens vandaag? wat is dit uberhaupt?
+
+    #initialisatie van BinaryNode
+    def __init__(self, lhs, rhs, op_symbol):
         self.lhs = lhs
         self.rhs = rhs
         self.op_symbol = op_symbol
     
-    # TODO: what other properties could you need? Precedence, associativity, identity, etc.
-            
+    #overloarden bij een gelijk teken
     def __eq__(self, other):
         if type(self) == type(other):
             return self.lhs == other.lhs and self.rhs == other.rhs
         else:
             return False
-    
-        #ik moet opnieuw de order of operation opstellen, dat moet sneller kunnen
-        
+   
+    #overloaden van de tostring functie
     def __str__(self):
         uitvoer = ""
         
-        zijde = 0
+        zijde = 0 #administreer of we de linker of rechter zijde bekijken (0 = lhs)
+        
+        #ga beide kanten langs en ga na of deze kant haakjes nodig heeft
         for side in [self.lhs, self.rhs]:
+            
+            # is het een binarynode? bepaal de operatie orde een laag naar beneden en de huidige operatieorde
+            # bepaal ook of de huidige operatie associatief is
             if isinstance(side, BinaryNode):
                 order_lower = self.order_op[side.op_symbol][0]
                 order_this = self.order_op[self.op_symbol][0]
-                lower_ass = self.order_op[self.op_symbol][1]
+                this_ass = self.order_op[self.op_symbol][1]
                 
-                if order_lower < order_this or (not lower_ass and order_lower <= order_this and zijde == 1):
+                #indien ofwel orde 1 laag dieper minder groot is dan de huidige, dan zijn haakjes nodig
+                #haakjes zijn ook nodig als de huidige operatie niet associatief is 
+                if order_lower < order_this or (not this_ass and order_lower <= order_this and zijde == 1):
                     uitvoer = uitvoer + "(%s)" % (str(side))
                 else:
                     uitvoer = uitvoer + str(side)
+            
+            # geen binarynode? Dan kan tostring worden aangeroepen
             else:
                 uitvoer = uitvoer + str(side)
-                
-            if side is self.lhs:
+            
+            # als de huidige zijde de lhs is, dan moet het operatiesymbool worden toegevoegd    
+            if zijde == 0:
                 uitvoer = uitvoer + " %s " % (self.op_symbol)
             
+            # een optellen om de rhs aan te duiden
             zijde = zijde + 1
             
         return uitvoer
 
+    #Evaluatie functie
     def evaluate(self, variabelen={}):
         
+        #bepaal de waarden van lhs en de rhs, neem daarin de ingevulde variable waarden mee
         getal1 = self.lhs.evaluate(variabelen)
         getal2 = self.rhs.evaluate(variabelen)
-        
-        if type(getal1) ==  str:
-            return getal1 + self.op_symbol +str(getal2)
-        elif type(getal2) == str:
-            return str(getal1) + self.op_symbol + getal2
+
+        #Als een van de twee géén constante is, dan is 1 van de twee ofwel een variabele, ofwel
+        #een compound expressie met een variabele er in. Dit kan dan niet als getal geevalueerd worden
+        if not isinstance(getal1,Constant):
+            return BinaryNode(getal1,getal2,self.op_symbol)
+        elif not isinstance(getal2,Constant):
+            return BinaryNode(getal1,getal2,self.op_symbol)
+            
+        #Wel twee constanten? Voer de operatie uit en maak een nieuwe constante aan
         else:
-            return Constant(eval('%s %s %s' % (getal1, self.op_symbol, getal2)))
+            return Constant(eval('%s %s %s' % (getal1.constantvalue(), self.op_symbol, getal2.constantvalue())))
         
-        
-        
-        # return ans
-        # if float(int(ans))==float(ans):
-        #     return int(ans)
-        # else:
-        #     return ans
-        # return Constant(eval('%s %s %s' % (getal1, self.op_symbol, getal2)))
-         
-        
-        
-        
-    #     getal1 = float(self.lhs)
-    #     getal2 = float(self.rhs)
-        
-    #     return ans = getal1 self.op_symbol getal2   
-       
-       
-       
+#Overloaden van de operaties     
 class AddNode(BinaryNode):
     """Represents the addition operator"""
     def __init__(self, lhs, rhs):
@@ -313,7 +307,3 @@ class PowNode(BinaryNode):
     """Represents the power operator"""
     def __init__(self, lhs, rhs):
         super(PowNode, self).__init__(lhs, rhs , '**')
-
-# TODO: add more subclasses of Expression to represent operators, variables, functions, etc.
-
-
