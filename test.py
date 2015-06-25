65b898861fb070253649e9a7b752d49bf8a62227
koenstemerdink <koenstemerdink@hotmail.com>
2015-06-25 13:13:27 +0000

    test

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ from expression import *
 a = Expression()
 
 
-#b=a.fromString("(5+((1+2)*4))-3") werkt
+b=a.fromString("x**2-5*x+4")
+c=a.fromString("0")
 #b=a.fromString("(5+((1+2)+4))-3") werkt
 #b=a.fromString("(5-((1+2)+4))-3")
 #b=a.fromString("((1/2)-(3/4))/(5*6)") werkt
 #b=a.fromString("12 * 13") werkt
+d=b.numSolver(b,c,'x',[-4,5])
 
-
-print(b)
+print(d)
